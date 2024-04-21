72ed4a52b14c7950882647d7556b0591b14e6a06
Ekaterina-Vilkova <e.a.vilkova88@gmail.com>
2024-04-21 20:23:02 +0300

    ADD and EDIT

--- workingWithArrays/TodoList/src/Main.java
+++ workingWithArrays/TodoList/src/Main.java
@@ -22,7 +22,18 @@ public class Main {
             if (word[0].equals("ADD")) {
                 String[] withoutFirstWord = Arrays.copyOfRange(word, 1, word.length);
                 String textWithoutFirstWord = Arrays.toString(withoutFirstWord).replaceAll("\\[|\\]|,", "");
-                if (withoutFirstWord.length > 0 && withoutFirstWord[0].matches("[0-9]")) {
+                if (todoList2.list.isEmpty()) {
+                    System.out.println("Список дел был пуст");
+                    String withoutIndex = textWithoutFirstWord.replaceAll(withoutFirstWord[0], "");
+                    todoList2.addNewDeal(null, withoutIndex);
+                    result = "Добавлено дело" + " \"" + withoutIndex.trim() + "\"" + " под номером 0";
+
+                } else if (withoutFirstWord[0].matches("[0-9]{1,}") && Integer.valueOf(withoutFirstWord[0]) > todoList2.list.size()) {
+                    String withoutIndex = textWithoutFirstWord.replaceAll(withoutFirstWord[0], "");
+                    todoList2.addNewDeal(null, withoutIndex);
+                    result = "Добавлено дело" + " \"" + withoutIndex.trim() + "\"" + " в конец списка";
+
+                } else if (withoutFirstWord.length > 0 && withoutFirstWord[0].matches("[0-9]{1,}")) {
                     String withoutIndex = textWithoutFirstWord.replaceAll(withoutFirstWord[0], "");
                     todoList2.addNewDeal(Integer.valueOf(withoutFirstWord[0]), withoutIndex);
                     result = "Добавлено дело" + " \"" + withoutIndex.trim() + "\" " + "под номером " + Integer.valueOf(withoutFirstWord[0]);
@@ -32,15 +43,19 @@ public class Main {
                 }
                 System.out.println(result);
             }
-            if (word[0].equals("EDIT") && word[1].matches("[0-9]")) {
-                String previousDeal = todoList2.list.get(Integer.valueOf(word[1]));
-                String[] withoutFirstWord = Arrays.copyOfRange(word, 2, word.length);
-                String textWithoutFirstWord = Arrays.toString(withoutFirstWord).replaceAll("\\[|\\]|,", "");
-                todoList2.editDeal(Integer.valueOf(word[1]), textWithoutFirstWord);
-                result = "Дело" + " \"" + previousDeal + "\" " + "заменено на " + "\"" + textWithoutFirstWord.trim() + "\" ";
-                System.out.println(result);
+            if (word[0].equals("EDIT") && word[1].matches("[0-9]{1,}")) {
+                if (Integer.valueOf(word[1]) > todoList2.list.size()) {
+                    System.out.println("Дело с таким номером не существует");
+                } else {
+                    String previousDeal = todoList2.list.get(Integer.valueOf(word[1]));
+                    String[] withoutFirstWord = Arrays.copyOfRange(word, 2, word.length);
+                    String textWithoutFirstWord = Arrays.toString(withoutFirstWord).replaceAll("\\[|\\]|,", "");
+                    todoList2.editDeal(Integer.valueOf(word[1]), textWithoutFirstWord);
+                    result = "Дело" + " \"" + previousDeal.trim() + "\" " + "заменено на " + "\"" + textWithoutFirstWord.trim() + "\" ";
+                    System.out.println(result);
+                }
             }
-            if (word.length == 2 && word[0].equals("DELETE") && word[1].matches("[1-9][0-9]{2}")) {
+            if (word.length == 2 && word[0].equals("DELETE") && word[1].matches("[0-9]{1,}")) {
                 int transmitted = Integer.parseInt(word[1]);
                 int all = todoList2.list.size();
                 if (transmitted > all) {
